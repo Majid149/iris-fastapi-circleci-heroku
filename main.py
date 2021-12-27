b7a16f6c9232e1336aa0ee5d9fc60cdef460492d
Majid149 <ahmedmajid149@gmail.com>
2021-12-27 21:28:57 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import uvicorn
+import pickle
+
+app = FastAPI()
+
+class iris(BaseModel):
+    a:float
+    b:float
+    c:float
+    d:float
+
+model = pickle.load(open('model_iris', 'rb'))
+
+@app.get("/")
+def home():
+    return {"msg": "ML model for IRIS PREDICTION"}
+
+@app.post('/make_predictions')
+async def make_prediction(features : iris):
+    return ({"prediction":str(model.predict([[features.a,features.b,features.c,features.d]])[0])})
